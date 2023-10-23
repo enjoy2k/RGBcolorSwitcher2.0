a31e4a2fc83467bd68d10b47a408ebaa984b5d83
enjoy2k <61755950+enjoy2k@users.noreply.github.com>
2023-10-23 18:57:30 +0300

    создал экран настроек

---
+++ RGBcolorSwitcher2/ViewControllers/SettingsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsViewController.swift
+//  RGBcolorSwitcher2
+//
+//  Created by Даниил Козлов on 23.10.2023.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
